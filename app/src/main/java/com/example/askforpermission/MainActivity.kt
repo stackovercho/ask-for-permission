41a2dc549f00e0da5947468c3722058cc1505438
Hyun <hyun.cho4@gmail.com>
2023-04-27 10:21:12 -0400

    added permission asking

--- app/src/main/java/com/example/askforpermission/MainActivity.kt
+++ app/src/main/java/com/example/askforpermission/MainActivity.kt
@@ -4,16 +4,22 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private var permission: String = Manifest.permission.CAMERA
+    private lateinit var launcher: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // check if permission to use the camera has already been granted
-        val grantedPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        val grantedPermission = ContextCompat.checkSelfPermission(this, permission)
 
         if (grantedPermission == PackageManager.PERMISSION_GRANTED) {
             // permission has been granted, start using the camera
@@ -21,6 +27,23 @@ class MainActivity : AppCompatActivity() {
         } else {
             // need to ask for permission to use camera
             Log.d("MainActivity", "permission was NOT previously granted")
+            val contract = ActivityResultContracts.RequestPermission()
+            // val result = Results()
+            // launcher = registerForActivityResult(contract, result)
+            launcher = registerForActivityResult(contract) {
+                if (it) Log.d("MainActivity", "permission granted!")
+                else Log.d("MainActivity", "sorry, permission not granted")
+            }
+            launcher.launch(permission)
+        }
+    }
+
+    inner class Results : ActivityResultCallback<Boolean> {
+        override fun onActivityResult(result: Boolean?) {
+            result?.let {
+                if (result) Log.d("MainActivity", "permission granted!")
+                else Log.d("MainActivity", "sorry, permission not granted")
+            }
         }
     }
 }
